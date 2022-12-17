6bf58b02b94a5066dbfeb3c95a7803e9ef3883d1
SCARRAIDER <rsahanediriweera@gmail.com>
2022-12-17 16:35:47 +0530

    added the functionals and functional calling

---
+++ functions/start.js
@@ -0,0 +1,13 @@
+// const math = require('./functions')
+
+// console.log(math.add(5,6));
+// console.log(math.sub(5,6));
+// console.log(math.mul(5,6));
+// console.log(math.div(5,6));
+
+const {add,sub,mul,div} = require('./functions');
+
+console.log(add(5,6));
+console.log(sub(5,6));
+console.log(mul(5,6));
+console.log(div(5,6));
