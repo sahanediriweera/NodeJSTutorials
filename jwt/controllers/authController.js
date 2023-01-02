66dbf12f5d0b3194fb7d94f29a0ff460a5e20495
SCARRAIDER <rsahanediriweera@gmail.com>
2023-01-02 08:53:01 +0530

    completed jwt

--- jwt/controllers/authController.js
+++ jwt/controllers/authController.js
@@ -3,6 +3,10 @@ const usersDB = {
     setUsers: function (data) { this.users = data }
 }
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const fsPromises = require('fs').promises;
+const path = require('path');
 
 const handleLogin = async (req, res) => {
     const { user, pwd } = req.body;
@@ -13,7 +17,35 @@ const handleLogin = async (req, res) => {
     const match = await bcrypt.compare(pwd, foundUser.password);
     if (match) {
         // create JWTs
-        res.json({ 'success': `User ${user} is logged in!` });
+        const accessToken = jwt.sign(
+            {
+                "username":foundUser.username
+            },
+            process.env.accessToken,
+            {
+                "expiresIn":'30s'
+            }
+        );
+
+        const refreshToken = jwt.sign(
+            {
+                "username":foundUser.username
+            },
+            process.env.refreshToken,
+            {
+                "expiresIn":'1d'
+            }
+        );
+
+        const otherUSers = usersDB.users.filter(person=> person.username !== foundUser.username);
+        const currentUser = {...foundUser,refreshToken};
+        usersDB.setUsers([...otherUSers,currentUser]);
+        await fsPromises.writeFile(
+            path.join(__dirname,'..','model','users.json'),
+            JSON.stringify(usersDB.users)
+            );
+        res.cookie('jwt',accessToken,{httpOnly:true,maxAge:24*60*60*1000});
+        res.json({accessToken});
     } else {
         res.sendStatus(401);
     }
---
+++ jwt/middleware/verifyJWT.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const verifyJWT  = (req,res,next) => {
+    const authHeader = req.headers['authorization'];
+    if(!authHeader){
+        return res.status(401);
+    }
+    console.log(authHeader);
+
+    const token = authHeader.split(' ')[1];
+    jwt.verify(
+        token,
+        process.env.ACCESS_TOKEN_SECRET,
+        (err,decoded)=> {
+            if(err) return res.sendStatus(401);
+            req.user = decoded.username;
+            next();
+        }
+    )
+};
+
+module.exports = verifyJWT;
