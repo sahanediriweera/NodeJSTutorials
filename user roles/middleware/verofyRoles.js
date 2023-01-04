c694e1500e42ecf79e0c6a93eca855dcb1601028
SCARRAIDER <rsahanediriweera@gmail.com>
2023-01-04 22:38:25 +0530

    completed the NodeJS

---
+++ user roles/middleware/verofyRoles.js
@@ -0,0 +1,17 @@
+const verifyRoles = (...allowedRoles)=> {
+    return (req,res,next)=> {
+        if(req?.roles){
+            return res.sendStatus(401);
+        }
+        const rolesArray = [...allowedRoles];
+        console.log(req.roles);
+        console.log(rolesArray);
+        const result = req.roles.map(role => rolesArray.includes(role)).find(val=> val=== true);
+        if(!results){
+            return res.sendStatus(401);
+        }
+        next();
+    }
+}
+
+module.exports = verifyRoles;
--- user roles/routes/api/employees.js
+++ user roles/routes/api/employees.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const employeesController = require('../../controllers/employeesController');
+const ROLES_LISt = require('./../../config/roles_list');
+const verifyRoles = require('./../../middleware/verofyRoles')
 
 router.route('/')
     .get(employeesController.getAllEmployees)
-    .post(employeesController.createNewEmployee)
-    .put(employeesController.updateEmployee)
-    .delete(employeesController.deleteEmployee);
+    .post(verifyRoles(ROLES_LISt.Admin,ROLES_LISt.Editor), employeesController.createNewEmployee)
+    .put(verifyRoles(ROLES_LISt.Admin,ROLES_LISt.Editor),employeesController.updateEmployee)
+    .delete(verifyRoles(ROLES_LISt.Admin),employeesController.deleteEmployee);
 
 router.route('/:id')
     .get(employeesController.getEmployee);
